d3f7cba640ba9726a61b579575f4a78cdd734f7b
mkcs4760 <mk4cf@mail.umsl.edu>
2019-05-02 15:24:29 -0500

    User process appears to be able to read clock from shared memory

--- user.c
+++ user.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/ipc.h> 
+#include <sys/shm.h>
 #include <sys/msg.h> 
 #include <string.h>
 #include <time.h>
@@ -14,8 +15,26 @@ int main(int argc, char *argv[]) {
 	
 	printf("Child %d is alive!!\n", getpid());
 	
+	int shmid;
+	key_t key = 1094;
+	int *clockSecs, *clockNano;
+	shmid = shmget(key, sizeof(int*) + sizeof(long*), 0666);
+	if(shmid < 0) {
+		perror("Shmget error in user process ");
+		exit(1);
+	}
+	//attach ourselves to that shared memory
+	clockSecs = shmat(shmid, NULL, 0); //attempting to store 2 numbers in shared memory
+	clockNano = clockSecs + 1;
+	if((clockSecs == (int *) -1) || (clockNano == (int *) -1)) {
+		perror("shmat error in user process");
+		exit(1);
+	}
+
 	
 	
-	printf("Child %d is shutting down\n", getpid());
+	
+	
+	printf("Child %d is shutting down at time %d:%d\n", getpid(), *clockSecs, *clockNano);
 	return 0;
 }

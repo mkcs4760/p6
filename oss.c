12c5ce275b61ef8d30b096b54cad782a8ea2a6aa
mkcs4760 <mk4cf@mail.umsl.edu>
2019-05-02 14:52:26 -0500

    Built frameTable for oss

--- oss.c
+++ oss.c
@@ -13,12 +13,32 @@
 #include <sys/time.h>
 #include "messageQueue.h"
 
-
+struct frame {
+	//a frame consists of a dirty bit, a reference byte, the process that has a page being stored here, and the page of that process that is being stored here
+	bool dirtyBit;
+	//bool referenceByte[8];
+	unsigned char referenceByte;
+	int processStored;
+	int pageStored;
+};
 
 int main(int argc, char *argv[]) {
 	
 	
-	printf("Hello world!\n");
+	printf("Welcome to Project 6\n");
+	
+	//we will need a frame table, as well as 18 page tables
+	struct frame frameTable[256];
+	int i;
+	printf("Frame#\tDB\tRB\tProcess:Page\n");
+	for (i = 0; i < 256; i++) {
+		//preset everything to starting values
+		frameTable[i].dirtyBit = false;
+		frameTable[i].referenceByte = 00000000;
+		frameTable[i].processStored = frameTable[i].pageStored = 0;
+		printf("%d\t%d\t%d\t%d:%d\n", i, frameTable[i].dirtyBit, frameTable[i].referenceByte, frameTable[i].processStored, frameTable[i].pageStored);
+	}
+	
 	
 	
 	
